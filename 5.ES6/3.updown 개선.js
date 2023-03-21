4fc6ecbc97142779e8d869d61f8e14d423677b4f
jhlee9462 <jhlee9462@gmail.com>
2023-03-21 12:46:41 +0900

    updown게임 개선

--- 5.ES6/3.updown 개선.js
+++ 5.ES6/3.updown 개선.js
@@ -1,65 +1,129 @@
+// 난이도를 선택하는 기능
+function selectGameLevel() {
 
-
-while (true) {
-  alert('난이도를 설정하세요~~~')
-  let difficulty; 
-  const HIGH = 1, MIDDLE = 2, LOW = 3;
-
-  let tryNum;
-  while (true) {
-      difficulty = +prompt('1. 상 (기회 3) 2. 중 (기회 6) 3. 겁쟁이들의 쉼터 (기회 10)');
-      if (difficulty < HIGH || difficulty > LOW || Math.floor(difficulty) !== difficulty) {
-          alert('1 ~ 3 사이의 정수를 입력해 주세요');
-          continue;
-      } else {
-          if (difficulty === HIGH) {
-              tryNum = 3;
-          } else if (difficulty === MIDDLE) {
-              tryNum = 6;
-          } else if (difficulty === LOW) {
-              tryNum = 10;
-          }
-          break;
-      }
-  }
-
-  const INIT_TRY_NUM = tryNum;
-
-  let minValue = 1, maxValue = 50;
-  let answer = Math.floor(Math.random() * maxValue + minValue);
-
+    const HIGH = 1,
+      MIDDLE = 2,
+      LOW = 3;
   
-  while (true) {
-
-      let userNum = +prompt(`${minValue} ~ ${maxValue} 사이의 무작위 숫자를 ${tryNum}번 안에 맞춰보세요`);
-
-      if (userNum < minValue || userNum > maxValue) {
-          alert(`${minValue} ~ ${maxValue} 사이의 숫자만 입력해 주세요`);
-          continue;
-      }
-
-      if (userNum > answer) {
-          alert('다운!!!');
-          maxValue = userNum - 1;
-      } else if (userNum < answer) {
-          alert('업!!!');
-          minValue = userNum + 1;
+    // 사용자의 초기 입력 기회
+    let INIT_COUNT;
+  
+    while (true) {
+      const level = +prompt('난이도를 선택하세요!\n# [1. 상(3번의 기회) | 2. 중(6번의 기회) | 3. 하(10번의 기회)]');
+  
+      if (level === HIGH) {
+        INIT_COUNT = 3;
+      } else if (level === MIDDLE) {
+        INIT_COUNT = 6;
+      } else if (level === LOW) {
+        INIT_COUNT = 10;
       } else {
-          alert(`정답!!! ${INIT_TRY_NUM - tryNum + 1} 번 만에 맞추셨네요!`);
-          break;
+        alert('난이도를 숫자로 다시 입력하세요!');
+        continue;
       }
-
-      alert(`기회가 ${--tryNum}번 남았습니다.`);
-      if (tryNum > 0) {
-          continue;
-      } else {
-          alert(`기회를 모두 소진했습니다. GAME OVER!!! 정답은 ${answer} 였습니다~~`);
-          break;
-      }
-  } // end each game loop
-
-  if (!confirm('게임을 다시 하실래요?')) {
-      alert('수고하셨습니다.')
-      break;
+      return INIT_COUNT;
+    }
   }
-} // end all game loop
+  
+  // 게임데이터를 생성하는 함수
+  function makeGameData() {
+  
+    const initCount = selectGameLevel();
+  
+    return {
+      secret: Math.floor(Math.random() * 100) + 1,
+      min: 1,
+      max: 100,
+      // 난이도를 선택하는 함수 호출
+      // 사용자의 남은 입력 기회
+      countDown: initCount,
+      // 사용자의 초기 입력 기회
+      initCount: initCount,
+      // 사용자의 입력값
+      answer: null,
+      // 게임 종료 플래그
+      gameEndFlag: false
+    };
+  }
+  
+  // 사용자가 답을 입력하고 검증하는 함수
+  function inputAnswerAndValidate(gameData) {
+    // 사용자 입력답
+    const answer 
+      = +prompt(`숫자를 입력하세요! [${gameData.min} ~ ${gameData.max}]`);
+  
+    // 입력이 범위안의 값인가?
+    if (answer < gameData.min || answer > gameData.max) {
+      alert('범위 안의 값을 입력하세요!');
+      return false;
+    }
+  
+    gameData.answer = answer;
+    return true;
+  }
+  
+  // 한 게임이 진행되는 함수
+  function runUpdownGame(gameData) {
+  
+    while (true) {
+          
+      if (!inputAnswerAndValidate(gameData)) continue;
+  
+      gameData.countDown--;
+  
+      if (gameData.secret === gameData.answer) {
+        alert(`정답입니다! ${gameData.initCount - gameData.countDown}번만에 맞췄습니다!`);
+        gameData.gameEndFlag = true;
+        break;
+      } else if (gameData.secret > gameData.answer) {
+        alert(`UP!!`);
+        gameData.min = gameData.answer + 1;
+      } else {
+        alert(`DOWN!!`);
+        gameData.max = gameData.answer - 1;
+      }
+  
+      // 추가 게임 종료 조건
+      if (gameData.countDown <= 0) {
+        alert(`응 너 졌어~~~ 정답은 ${gameData.secret}이었음ㅋㅋㅋ`);
+        gameData.gameEndFlag = true;
+        break;
+      } else {
+        alert(`${gameData.countDown}번의 기회가 남았습니다.`);
+      }
+  
+    } // end each game loop
+  }
+  
+  // 게임 시작 함수
+  function startGame() {
+  
+    while (true) {
+  
+      // 게임데이터 생성
+      const gameData = makeGameData();
+  
+      // 하나의 게임 실행
+      runUpdownGame(gameData);
+  
+  
+      if (gameData.gameEndFlag) {
+        const exitFlag = confirm('한 판더?');
+        if (!exitFlag) {
+          alert('수고하셨습니다!');
+          return;
+        }
+      }
+  
+    } // end all game loop
+  
+  }
+  
+  
+  
+  //=========================================//
+  
+  
+  // 게임 실행!!!
+  
+  startGame();
